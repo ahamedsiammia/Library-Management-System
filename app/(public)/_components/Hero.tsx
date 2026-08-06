3f6609f399837b79845d8151beb0e3431f50d28a
ahamedsiammia <md7559263@gmail.com>
2026-08-06 22:32:36 +0600

    solved some issues

--- app/(public)/_components/Hero.tsx
+++ app/(public)/_components/Hero.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 
-// import "swiper/css";
-// import "swiper/css/pagination";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Hero: React.FC = () => {
   return (
